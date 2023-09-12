c8916de471c64881e52ea59e62d6e6caea849630
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-09-13 02:58:21 +0300

    delete header, tabbar

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export * from "./RegistrationForm";
 export * from "./LoginForm";
-export * from "./Header";
-export * from "./TabBar";
+// export * from "./Header";
+// export * from "./TabBar";
 export * from "./ToolBar";
 export * from "./ContentBox";
 export * from "./ContentScrollBox";
