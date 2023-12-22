dd5802ece69a307562585e769b76b51aa1489a27
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-12-22 02:32:34 +0200

    change const img

--- src/data/const.js
+++ src/data/const.js
@@ -1,4 +1,3 @@
 export const AVATAR_DEFAULT = "../Img/avatar_default.png";
 
-export const avatarNothing =
-  "https://res.cloudinary.com/de7gxd2bv/image/upload/v1700568484/PowerPulse/Avatars/655c9ab08dc0dc70ac5ba6c7_avatar.jpg";
+export const avatarNothing = "https://reactnative.dev/img/tiny_logo.png";
