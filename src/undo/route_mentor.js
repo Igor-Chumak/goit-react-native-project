10e95f8464105be7dbc55e1c6412cb47b474c178
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-12-24 22:38:32 +0200

    reafact routes

--- src/undo/route_mentor.js
+++ src/undo/route_mentor.js
@@ -1,12 +1,12 @@
 import { createStackNavigator } from "@react-navigation/stack";
 
-import RegistrationScreen from "./Screens/RegistrationScreen";
-import LoginScreen from "./Screens/LoginScreen";
-import CommentsScreen from "./Screens/CommentsScreen";
-import HomeScreen from "./Screens/HomeScreen";
-import MapScreen from "./Screens/MapScreen";
+import RegistrationScreen from "../Screens/RegistrationScreen";
+import LoginScreen from "../Screens/LoginScreen";
+import CommentsScreen from "../Screens/CommentsScreen";
+import HomeScreen from "../Screens/HomeScreen";
+import MapScreen from "../Screens/MapScreen";
 
-import { GoBackIconBox, HeaderTitle } from "./components";
+import { GoBackIconBox, HeaderTitle } from "../components";
 
 const AuthStack = createStackNavigator();
 
