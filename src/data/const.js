97ce7873649b14b5c1f7573c7d233eff40194461
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-12-26 17:16:38 +0200

    Update const

--- src/data/const.js
+++ src/data/const.js
@@ -1,3 +1,4 @@
 export const AVATAR_DEFAULT = "../images/avatar_default.png";
 
-export const avatarNothing = "https://reactnative.dev/img/tiny_logo.png";
+// export const avatarNothing = "https://reactnative.dev/img/tiny_logo.png";
+export const avatarNothing = "gs://reactnative-goit-e3765.appspot.com/avatars/tiny_logo.png";
